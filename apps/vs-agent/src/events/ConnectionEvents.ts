7966b127c98fa715707aa7e8a68bd417b878bce0
Ariel Gentile <gentilester@gmail.com>
2026-03-11 17:03:38 -0300

    fix: speed-up docker image startup (#363)
    Signed-off-by: Ariel Gentile <gentilester@gmail.com>

--- apps/vs-agent/src/events/ConnectionEvents.ts
+++ apps/vs-agent/src/events/ConnectionEvents.ts
@@ -19,7 +19,7 @@ import { sendWebhookEvent } from './WebhookEvent'
 export const connectionEvents = async (agent: VsAgent, config: ServerConfig) => {
   // Get the first record matching agent's DID and obtain all alternatives for it
   const [agentPublicDidRecord] = await agent.dids.getCreatedDids({ did: agent.did })
-  const alternativeDids = agentPublicDidRecord.getTag('alternativeDids')
+  const alternativeDids = agentPublicDidRecord?.getTag('alternativeDids')
   const agentPublicDids = [agent.did, ...(Array.isArray(alternativeDids) ? alternativeDids : [])]
 
   agent.events.on(
