e7ccd8bd79418fd9d919ebcd2a424d2a0fc1880a
andres vallecilla <andresfelipe083195@hotmail.com>
2026-03-24 11:35:57 -0500

    fix: vs agent main page add verana icon
    Signed-off-by: andres vallecilla <andresfelipe083195@hotmail.com>

--- apps/vs-agent-ui/src/components/Header.jsx
+++ apps/vs-agent-ui/src/components/Header.jsx
@@ -1,8 +1,10 @@
+import logoSvg from '../assets/logo.svg'
+
 export default function Header() {
   return (
     <header className="header">
       <div className="header-logo">
-        <div className="header-logo-icon">V</div>
+        <img src={logoSvg} alt="Verana" className="header-logo-icon" />
         <span className="header-logo-name">Verana</span>
       </div>
     </header>
