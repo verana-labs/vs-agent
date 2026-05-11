fe696575f817c579b95a9ff66507a1a75af10937
Ariel Gentile <gentilester@gmail.com>
2026-05-11 19:59:04 -0300

    test: cleanup test files
    Signed-off-by: Ariel Gentile <gentilester@gmail.com>

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,16 @@
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
 import { defineConfig } from "vitest/config";
+
+const dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   test: {
     environment: "node",
     passWithNoTests: true,
     clearMocks: true,
+    globalSetup: [path.resolve(dirname, "vitest.globalSetup.ts")],
     coverage: {
       provider: "v8",
       reportsDirectory: "./coverage",
