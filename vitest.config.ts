401ccc6fd6c05af80ed186982d56055a5ad86b60
Ariel Gentile <gentilester@gmail.com>
2026-05-12 09:59:50 -0300

    fix: lint error
    Signed-off-by: Ariel Gentile <gentilester@gmail.com>

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,16 +1,13 @@
 import path from "node:path";
-import { fileURLToPath } from "node:url";
 
 import { defineConfig } from "vitest/config";
-
-const dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   test: {
     environment: "node",
     passWithNoTests: true,
     clearMocks: true,
-    globalSetup: [path.resolve(dirname, "vitest.globalSetup.ts")],
+    globalSetup: [path.resolve(__dirname, "vitest.globalSetup.ts")],
     coverage: {
       provider: "v8",
       reportsDirectory: "./coverage",
